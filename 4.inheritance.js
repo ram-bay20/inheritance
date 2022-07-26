efbf62afbbd23dc889731de4d6f3cb47640e5d82
Ramnath Mandal <107981223+ram-bay20@users.noreply.github.com>
2022-07-26 10:55:45 +0545

    Add files via upload

--- 4.inheritance.js
+++ 4.inheritance.js
@@ -16,6 +16,11 @@ function Magazine(title,author,year,month){
      this.month = month;
 }
 
+Magazine.prototype.summary = function(){
+     return `${this.title} was written by ${this.author} in ${this.month} ${this.year}.`;
+}
+
+
 Magazine.prototype.constuctor = Object.create(Book.prototype);
 
 const mag1 = new Magazine('Hamlet', 'William Shakespear', '1540', 'march');
@@ -23,3 +28,4 @@ const mag1 = new Magazine('Hamlet', 'William Shakespear', '1540', 'march');
 Magazine.prototype.constuctor = Magazine;
 
 console.log(mag1);
+console.log(mag1.summary());
